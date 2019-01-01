3e120263df32d329fade5d170eb6425bb0d81eb8
Dani Gámez Franco <gmzcodes@outlook.com>
2019-01-01 12:18:14 +0100

    Adds more symbol sets.

--- src/app/constants/symbols.constants.js
+++ src/app/constants/symbols.constants.js
@@ -1,17 +1,28 @@
 export const SYMBOLS_CLASSIC = [
-    '💀', '🍋', '🍊', '🍉', '🍇', '🍓', '🍒', '🔔', '🍀', '💎'];
+    '🍋', '🍊', '🍉', '🍈', '🍇', '🥝', '🍓', '🍒', '🌟', '🍀', '💎', '🎰'];
 
 export const SYMBOLS_CHRISTMAS = [
-    '⛄', '🦌', '🎄', '🎀', '🎁', '🎆', '🍾', '🔔', '🎅', '🌟'];
+    '🧣', '⛄', '🎄', '🎁', '🎀', '🍾', '🍷', '🔔', '🌟', '🦌', '🎅'];
 
 export const SYMBOLS_HALLOWEEN = [
-    '🌚', '🐺', '🎃', '🧠', '👹', '👽', '👻', '🧟‍', '🧛‍', '💀'];
+    '🌚', '🎃', '🦇', '😈', '🧠', '👹', '👽', '🤡', '👻', '🧟‍', '🧛‍', '💀'];
 
 export const SYMBOLS_ANIMALS = [
-    '🐭', '🐱', '🦀', '🐞', '🐓', '🐍', '🐟', '🐸', '🦊‍', '🦁'];
+    '🐞', '🐟', '🐭', '🐸', '🐓', '🐷', '🦀', '🐍', '🦊‍', '🦁', '🦕', '🦖'];
 
 export const SYMBOLS_SPORTS = [
-    '⚽', '🏀', '🏈', '⚾', '🎾', '🏐', '🏉', '🎱', '🥊‍', '🎯'];
+    '🎯', '🎱', '🏐', '⛳', '⚾', '🏈', '🏀', '⚽', '🥉', '🥈', '🥇', '🏆'];
+
+export const SYMBOLS_LOVE = [
+    '💌', '🎁', '📸', '💘', '💋', '🔞', '💑', '💍', '💎', '💖',
+];
+
+export const SYMBOLS_FORTUNE = [
+    '🎴', '🎲', '🕶', '🥃', '💷', '💴', '💵', '💰', '🍀', '💎', '🎰', '🧞‍️', '🃏'];
+
+export const SYMBOLS_FOOD = [
+    '🥦', '🍋', '🍆', '🍙', '🍯', '🍑', '🍓', '🍭', '🍫', '🍩', '🍕', '🍗', '🥩', '🍣',
+];
 
 export const ALL_SYMBOLS = [
     SYMBOLS_CLASSIC,
@@ -19,6 +30,9 @@ export const ALL_SYMBOLS = [
     SYMBOLS_HALLOWEEN,
     SYMBOLS_ANIMALS,
     SYMBOLS_SPORTS,
+    SYMBOLS_LOVE,
+    SYMBOLS_FORTUNE,
+    SYMBOLS_FOOD,
 ];
 
 export const SYMBOLS_RANDOM = ALL_SYMBOLS[Math.random() * ALL_SYMBOLS.length | 0];
