5a3cab72618078d824f7abeb6715e36862173017
Dani Gámez Franco <gmzcodes@outlook.com>
2019-01-02 17:38:37 +0100

    Adds babel polyfill.

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,6 @@
+/* eslint-disable sort-imports */
+import '@babel/polyfill'; // This one needs to be at the top!
+
 import { App } from './components/app/app.component';
 
 // eslint-disable-next-line no-new
